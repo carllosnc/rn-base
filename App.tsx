55ace19cdd07715eb13c167abeb850339549e4be
Carlos Costa <carllos.nc@gmail.com>
2025-11-10 19:46:10 -0300

    feat(navigation): switch from NativeStack to DrawerNavigator for improved navigation structure

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
 import './global.css'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { createStaticNavigation } from '@react-navigation/native'
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { HomePage } from 'pages/home'
+import { Drawer } from 'pages/drawer';
+import colors from 'settings/colors.json'
 
-const RootStack = createNativeStackNavigator({
+const RootStack = createDrawerNavigator({
   screenOptions: {
+    drawerHideStatusBarOnOpen: true,
+    drawerStyle: {
+      backgroundColor: colors.base,
+      borderTopEndRadius: 0,
+      borderBottomEndRadius: 0,
+      shadowRadius: 0,
+      borderRadius: 0
+    },
     headerShown: false
+  },
+  drawerContent(props) {
+    return (<Drawer {...props} />)
   },
   screens: {
     Home: {
