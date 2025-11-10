fd0e6448ff3c5f9fd5ad923f176e21300fcc274b
Carlos Costa <carllos.nc@gmail.com>
2025-11-10 19:46:35 -0300

    feat(top-bar): add drawer button for improved navigation access

--- components/top-bar.tsx
+++ components/top-bar.tsx
@@ -1,25 +1,29 @@
 import { Ionicons } from '@expo/vector-icons';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { IconButton } from './icon-buttons';
 import { ReactNode } from 'react';
 import { clsx } from 'clsx';
+import { useNavigation } from '@react-navigation/native';
 
 type TopBarProps = {
-  title: string;
-  titleLimitChars?: number;
-  titleClass?: string;
-  topBarClass?: string;
-  backButton?: boolean;
-  actions?: ReactNode[];
+  title: string
+  titleLimitChars?: number
+  titleClass?: string
+  topBarClass?: string
+  backButton?: boolean
+  drawerButton?: boolean
+  actions?: ReactNode[]
 };
 
 export function TopBar({
   title,
   titleClass,
   topBarClass,
+  drawerButton = false,
   titleLimitChars = 30,
   backButton = false, actions,
   }: TopBarProps) {
+  const navigation = useNavigation()
 
   function getTitle() {
     if (title.length > titleLimitChars) {
@@ -34,18 +38,29 @@ export function TopBar({
   );
 
   const topBarClasses = clsx(
-    `bg-base flex h-[60px] w-full flex-row items-center justify-between px-[12px]`,
+    `bg-base flex h-[60px] w-full flex-row items-center justify-between px-[12px] pl-[20px]`,
     topBarClass
   )
 
   return (
     <View className={topBarClasses}>
       <View className="flex flex-row items-center gap-[10px]">
+
+        { drawerButton && (
+          <TouchableOpacity
+            className='py-[5px]'
+            activeOpacity={0.7}
+            onPress={() => { (navigation as any).openDrawer() }}>
+            <Ionicons name="menu-outline" size={32} />
+          </TouchableOpacity>
+        )}
+
         {backButton && (
           <IconButton>
             <Ionicons name="chevron-back" size={18} />
           </IconButton>
         )}
+
         <View className="flex h-[30px] justify-center">
           <Text className={titleClasses}>{getTitle()}</Text>
         </View>
