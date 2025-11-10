22d5116d096528eb7113f60ead3a3864bf10b465
Carlos Costa <carllos.nc@gmail.com>
2025-11-10 08:56:13 -0300

    feat(top-bar): add TopBar component with customizable title, back button, and action support

---
+++ components/top-bar.tsx
@@ -0,0 +1,59 @@
+import { Ionicons } from '@expo/vector-icons';
+import { View, Text } from 'react-native';
+import { IconButton } from './icon-buttons';
+import { ReactNode } from 'react';
+import { clsx } from 'clsx';
+
+type TopBarProps = {
+  title: string;
+  titleLimitChars?: number;
+  titleClass?: string;
+  topBarClass?: string;
+  backButton?: boolean;
+  actions?: ReactNode[];
+};
+
+export function TopBar({
+  title,
+  titleClass,
+  topBarClass,
+  titleLimitChars = 30,
+  backButton = false, actions,
+  }: TopBarProps) {
+
+  function getTitle() {
+    if (title.length > titleLimitChars) {
+      return `${title.slice(0, titleLimitChars)}...`;
+    }
+    return title;
+  }
+
+  const titleClasses = clsx(
+    'truncate text-[16px] font-bold',
+    titleClass
+  );
+
+  const topBarClasses = clsx(
+    `bg-base flex h-[60px] w-full flex-row items-center justify-between px-[12px]`,
+    topBarClass
+  )
+
+  return (
+    <View className={topBarClasses}>
+      <View className="flex flex-row items-center gap-[10px]">
+        {backButton && (
+          <IconButton>
+            <Ionicons name="chevron-back" size={18} />
+          </IconButton>
+        )}
+        <View className="flex h-[30px] justify-center">
+          <Text className={titleClasses}>{getTitle()}</Text>
+        </View>
+      </View>
+
+      <View className="flex flex-row items-center gap-[5px]">
+        {actions?.map((item, index) => item)}
+      </View>
+    </View>
+  );
+}
