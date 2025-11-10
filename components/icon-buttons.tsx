4284cdf56475ff1ccf72834d4928bcb19e1a54d7
Carlos Costa <carllos.nc@gmail.com>
2025-11-10 08:56:03 -0300

    feat(icon-button): add IconButton component for flexible icon button usage

---
+++ components/icon-buttons.tsx
@@ -0,0 +1,19 @@
+import { clsx } from "clsx";
+import { ReactNode } from "react";
+import { TouchableOpacityProps, TouchableOpacity } from "react-native";
+
+interface IconButtonProps extends TouchableOpacityProps {
+  className?: string
+  children: ReactNode
+}
+
+export function IconButton({ className, children, ...props }: IconButtonProps){
+  return  (
+    <TouchableOpacity
+      className={clsx("flex h-[34px] w-[34px] items-center justify-center rounded-full", className)}
+      {...props}
+    >
+      { children }
+    </TouchableOpacity>
+  )
+}
