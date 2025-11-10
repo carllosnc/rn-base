c541e58aa0f4ec844ce41909683c964cf4101f03
Carlos Costa <carllos.nc@gmail.com>
2025-11-10 08:55:56 -0300

    feat(tailwind): add page content support and extend color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+import colors from './settings/colors.json'
+
 module.exports = {
-  content: ['./App.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
+  content: [
+    './App.{js,ts,tsx}',
+    './components/**/*.{js,ts,tsx}',
+    './pages/**/*.{js,ts,tsx}',
+  ],
 
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': colors.primary,
+        'base': colors.base,
+        'primary-content': colors.primaryContent
+      }
+    },
   },
   plugins: [],
-};
+}
