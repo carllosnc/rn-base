d00d502a1aac6c9923370ac774a18c4cebe17f46
Carlos Costa <carllos.nc@gmail.com>
2025-11-30 18:50:14 -0300

    fix(drawer): improve layout and text styling in Drawer component

--- pages/drawer.tsx
+++ pages/drawer.tsx
@@ -1,17 +1,19 @@
-import { DrawerContentComponentProps } from "@react-navigation/drawer";
-import { View, Text } from "react-native";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { DrawerContentComponentProps } from '@react-navigation/drawer';
+import { View, Text } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
-export function Drawer({ navigation }: DrawerContentComponentProps){
+export function Drawer({ navigation }: DrawerContentComponentProps) {
   return (
     <SafeAreaProvider>
-      <SafeAreaView className="p-[20px]">
+      <SafeAreaView className={'flex-1 bg-white p-[20px] dark:bg-neutral-800'}>
         <View>
-          <Text>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.Provident tempore dolore quisquam sunt impedit quos a tenetur amet ratione animi? Incidunt nisi a mollitia enim assumenda autem facere animi doloremque?
+          <Text className="text-black dark:text-white">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.Provident tempore dolore
+            quisquam sunt impedit quos a tenetur amet ratione animi? Incidunt nisi a mollitia enim
+            assumenda autem facere animi doloremque?
           </Text>
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
-  )
+  );
 }
