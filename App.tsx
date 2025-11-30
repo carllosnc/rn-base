eb475ae06593e70655564a764dde98cb96d2331b
Carlos Costa <carllos.nc@gmail.com>
2025-11-30 18:49:27 -0300

    fix(app): remove background color from drawer style for improved flexibility

--- App.tsx
+++ App.tsx
@@ -3,13 +3,11 @@ import { createStaticNavigation } from '@react-navigation/native'
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { HomePage } from 'pages/home'
 import { Drawer } from 'pages/drawer';
-import colors from 'settings/colors.json'
 
 const RootStack = createDrawerNavigator({
   screenOptions: {
     drawerHideStatusBarOnOpen: true,
     drawerStyle: {
-      backgroundColor: colors.base,
       borderTopEndRadius: 0,
       borderBottomEndRadius: 0,
       shadowRadius: 0,
