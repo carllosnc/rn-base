f6ed402733349b522bbbd79c4d98bc30d4da59e4
Carlos Costa <carllos.nc@gmail.com>
2025-12-01 21:29:20 -0300

    fix(text-field): correct import path for P component and update textInputStyle logic

--- components/text-field.tsx
+++ components/text-field.tsx
@@ -1,7 +1,7 @@
 import { clsx } from 'clsx';
 import { TextInput, View } from 'react-native';
 import { type TextInputProps } from 'react-native';
-import { P } from './text';
+import { P } from './p';
 
 interface TextFieldProps extends TextInputProps {
   label?: string;
@@ -22,6 +22,7 @@ export function TextField({
     "px-[12px] py-[10px] border border-neutral-400 dark:border-neutral-600 rounded-[8px] text-black dark:text-white",
     "placeholder:text-neutral-400 dark:placeholder:text-neutral-500",
     { "border-red-600 bg-red-50 dark:bg-neutral-900 dark:border-red-300": isError },
+    { "bg-neutral-200 dark:bg-neutral-700": props.editable === false },
     className
   );
 
