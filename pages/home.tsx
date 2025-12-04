e50b74f465df0a04fd6b9b3712eb9cc113146788
Carlos Costa <carllos.nc@gmail.com>
2025-12-04 13:45:15 -0300

    feat(home): enhance home page ui with components

--- pages/home.tsx
+++ pages/home.tsx
@@ -11,65 +11,42 @@ import { P } from 'components/p'
 import { ListTile } from 'components/list-tile'
 import { Dialog } from 'components/dialog'
 import { BottomSheet } from 'components/bottom-sheet'
+import { ParamListBase, useNavigation } from '@react-navigation/native'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 
 function HomeTopBar(){
   const { contentColor } = useTheme()
 
   return (
     <TopBar
-      drawerButton={true}
       actions={[
         <IconButton key={key()} onPress={() => {}}>
-          <Ionicons name="bluetooth-sharp" size={20} color={contentColor} />
-        </IconButton>,
-        <IconButton key={key()} onPress={() => {}}>
-          <Ionicons name="airplane-outline" size={20} color={contentColor} />
-        </IconButton>,
-        <IconButton key={key()} onPress={() => {}}>
-          <Ionicons name="american-football-outline" size={20} color={contentColor} />
-        </IconButton>,
-        <IconButton key={key()} onPress={() => {}}>
-          <Ionicons name="bag-handle-outline" size={20} color={contentColor} />
+          <Ionicons name="notifications-outline" size={20} color={contentColor} />
         </IconButton>,
       ]}
       titleLimitChars={20}
-      title="this is another item lets work with that"
+      title="RN Base"
     />
   )
 }
 
 export function HomePage() {
   const iconColor = useColor({ light: "black", dark: "white" })
+  const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>()
 
   return (
     <Scaffold
       topBar={<HomeTopBar/>}
       className="bg-white dark:bg-neutral-800 w-full h-full relative"
       >
-      <View className="gap-[30px] p-[20px] pb-[80px]">
-        <P>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi aliquid alias quae molestias at repellat inventore rerum cumque perferendis, animi ullam, autem consequuntur debitis reiciendis voluptatibus ipsam a cum nemo?
-        </P>
+      <View className="gap-[30px] pb-[80px]">
 
-        <Dialog
-          contentClass="p-[30px]"
-          target={<Button disabled> Open modal </Button>}
-        >
-          <P className="font-bold text-[20px]"> Modal title </P>
-          <P className="text-neutral-700">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </P>
-        </Dialog>
-
-        <BottomSheet
-          contentClass="p-[30px]"
-          target={<Button disabled> Open Bottom Sheet </Button>}
-        >
-          <P className="font-bold text-[20px]"> Modal title </P>
-          <P className="text-neutral-700">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </P>
-        </BottomSheet>
+        <View className="px-[20px] gap-[3px]">
+          <P className="text-[15px]">Lorem Ipsum is simply</P>
+          <P className="text-[20px]">Lorem Ipsum is simply</P>
+          <P className="text-[25px]">Lorem Ipsum is simply</P>
+          <P className="text-[30px]">Lorem Ipsum is simply</P>
+        </View>
 
         <View>
           <ListTile
@@ -92,7 +69,7 @@ export function HomePage() {
           />
         </View>
 
-        <View className="flex-row flex flex-wrap gap-[10px]">
+        <View className="px-[20px] flex-row flex flex-wrap gap-[10px]">
           <Button> Button </Button>
           <Button leftIcon={ <Ionicons name="bag-handle" size={16} color="white" /> }>
               Button with icon
@@ -100,7 +77,7 @@ export function HomePage() {
           <Button isLoading> Loading </Button>
         </View>
 
-        <View className="flex-row gap-[20px]">
+        <View className="px-[20px] flex-row gap-[20px]">
           <IconButton className="bg-blue-600 w-[50px] h-[50px]" key={key()} onPress={() => {}}>
             <Ionicons name="bag-handle" size={25} color="white" />
           </IconButton>
@@ -115,34 +92,45 @@ export function HomePage() {
           </IconButton>
         </View>
 
-        <TextField isError label="Label" placeholder="Placeholder" />
+        <View className="px-[20px] gap-[20px]">
+          <TextField isError label="Label" placeholder="Placeholder" />
 
-        <TextField label="Label" placeholder="Disabled text field" editable={false} />
+          <TextField label="Label" placeholder="Disabled text field" editable={false} />
 
-        <TextField label="Label" placeholder="Placeholder" />
+          <TextField label="Label" placeholder="Placeholder" />
 
-        <TextField
-          label="Label"
-          placeholder="Placeholder"
-          errorMessage="this is a error message"
-        />
+          <TextField
+            label="Label"
+            placeholder="Placeholder"
+            errorMessage="this is a error message"
+          />
+        </View>
 
-        <View className="gap-[10px]">
-          <View className="gap-[10px] flex-row">
-            <TextField placeholder="Placeholder" />
-            <TextField placeholder="Placeholder" />
-          </View>
-          <View className="gap-[10px] flex-row">
-            <TextField placeholder="Placeholder" />
-            <TextField placeholder="Placeholder" />
-            <TextField placeholder="Placeholder" />
-          </View>
-          <View className="gap-[10px] flex-row">
-            <TextField placeholder="Placeholder" />
-            <TextField placeholder="Placeholder" />
-            <TextField placeholder="Placeholder" />
-            <TextField placeholder="Placeholder" />
-          </View>
+        <View className="px-[20px] flex-row gap-[10px]">
+          <Dialog
+            contentClass="p-[30px]"
+            target={<Button disabled> Open modal </Button>}
+          >
+            <P className="font-bold text-[20px]"> Modal title </P>
+            <P className="text-neutral-700">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </P>
+          </Dialog>
+          <BottomSheet
+            contentClass="p-[30px]"
+            target={<Button disabled> Open Bottom Sheet </Button>}
+          >
+            <P className="font-bold text-[20px]"> Modal title </P>
+            <P className="text-neutral-700">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </P>
+          </BottomSheet>
+        </View>
+
+        <View className="px-[20px]">
+          <Button onPress={() => navigation.navigate("About" as never) }>
+            Navigation
+          </Button>
         </View>
 
       </View>
