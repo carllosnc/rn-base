c36d1136902e0ac8f55812c743536ff6facf5b0a
Carlos Costa <carllos.nc@gmail.com>
2025-12-08 00:52:19 -0300

    Update bottom sheet component

--- components/bottom-sheet.tsx
+++ components/bottom-sheet.tsx
@@ -1,18 +1,42 @@
-import { View, Modal, Pressable } from "react-native";
-import { ReactElement, ReactNode, useState } from "react";
+import { View, Modal, Pressable } from "react-native"
+import { ReactElement, ReactNode, useState } from "react"
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated';
 import { clsx } from "clsx";
+import { scheduleOnRN } from "react-native-worklets";
 
 interface BottomSheetProps {
   target: ReactElement
   children: ReactNode
   containerClass?: string,
   contentClass?: string
+  height?: number
 }
 
 export function BottomSheet(
-  { target, children, containerClass, contentClass }: BottomSheetProps
+  { target, children, containerClass, contentClass, height = 700 }: BottomSheetProps
 ){
+  const translateY = useSharedValue(height)
   const [modalVisible, setModalVisible] = useState(false)
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return { transform: [{ translateY: translateY.value }] }
+  })
+
+  function show(){
+    setModalVisible(true)
+    translateY.value = withSpring(0)
+  };
+
+  function hide(){
+    translateY.value = withTiming(height, {duration: 300}, (isFinished) => {
+      if(isFinished) scheduleOnRN(setModalVisible, false)
+    })
+  }
 
   const containerStyle = clsx(
     "bg-black/75 blur-lg h-full w-fit flex justify-end",
@@ -20,32 +44,33 @@ export function BottomSheet(
   )
 
   const contentStyle = clsx(
-    "w-full p-[20px] gap-[10px] rounded-tl-[20px] rounded-tr-[20px] min-h-[80%] max-h-full bg-white dark:bg-neutral-800",
+    "w-full gap-[10px] rounded-tl-[20px] rounded-tr-[20px] max-h-full bg-white dark:bg-neutral-800",
     contentClass
   )
 
   return (
     <View>
       <Modal
+        allowSwipeDismissal={true}
         animationType="fade"
         transparent={true}
         visible={modalVisible}
         hardwareAccelerated={true}
-        statusBarTranslucent={true}
         onRequestClose={() => {
-          setModalVisible(!modalVisible)
-        }}>
-        <Pressable onPress={() => { setModalVisible(false) }}  className={containerStyle}>
-          <Pressable className={contentStyle}>
-            { children }
+          hide()
+        }}
+        statusBarTranslucent={true}>
+        <Pressable onPress={hide}  className={containerStyle}>
+          <Pressable>
+            <Animated.View className={contentStyle} style={[{height: height}, animatedStyle]}>
+              {children}
+            </Animated.View>
           </Pressable>
         </Pressable>
       </Modal>
 
       <Pressable
-        onPress={() => {
-          setModalVisible(true)
-        }}
+        onPress={show}
       >
         {target}
       </Pressable>
